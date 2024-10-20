dfa7f90d4b78eaff86445f8de7a681ff05297f65
Dandi Pangestu <dandipangestu@MEKARIs-MacBook-Pro-90.local>
2024-10-21 00:48:37 +0700

    Publish message to sqs for each extracted email

--- extract-email-function/main.go
+++ extract-email-function/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"context"
 	"encoding/csv"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"io"
 	"log"
 )
+
+type messageBody struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+var sendMessageQueueURL = "https://sqs.us-east-1.amazonaws.com/116981782358/send-email"
 
 func handler(ctx context.Context, s3Event events.S3Event) error {
 	sdkConfig, err := config.LoadDefaultConfig(ctx)
@@ -18,6 +27,7 @@ func handler(ctx context.Context, s3Event events.S3Event) error {
 		return err
 	}
 	s3Client := s3.NewFromConfig(sdkConfig)
+	sqsClient := sqs.NewFromConfig(sdkConfig)
 
 	for _, record := range s3Event.Records {
 		bucket := record.S3.Bucket.Name
@@ -41,13 +51,34 @@ func handler(ctx context.Context, s3Event events.S3Event) error {
 				break
 			} else if err != nil {
 				log.Printf("failed when reading data from csv: %s\n", err)
-				break
+				return err
 			}
 			if !isHeaderFetched {
 				isHeaderFetched = true
 				continue
 			}
-			log.Printf("===== email: %s, name: %s =====\n", data[0], data[1])
+			email, name := data[0], data[1]
+			log.Printf("===== email: %s, name: %s =====\n", email, name)
+
+			bodyBytes, err := json.Marshal(&messageBody{
+				Email: email,
+				Name:  name,
+			})
+			if err != nil {
+				log.Printf("failed to encode the object with email %s: %s\n", email, err)
+				continue
+			}
+			bodyStr := string(bodyBytes)
+
+			resp, err := sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+				QueueUrl:    &sendMessageQueueURL,
+				MessageBody: &bodyStr,
+			})
+			if err != nil {
+				log.Printf("failed to publish queue with email %s: %s\n", email, err)
+				continue
+			}
+			log.Printf("message published for email %s with message id %s\n", email, *resp.MessageId)
 		}
 	}
 
